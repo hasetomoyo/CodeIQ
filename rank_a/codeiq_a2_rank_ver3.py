e42c4c20725a859316149235c65aa018fc49ad5d
hasegawa tomoyo <31061799+hasetomoyo@users.noreply.github.com>
2017-08-21 17:22:22 +0900

    パスした最終解答
    周期性を事前に求めて、格納しておく
    周期性を求めるために使用したプログラム(ただ100こ出力するだけ)を同ディレクトリに格納→sample.py

---
+++ rank_a/codeiq_a2_rank_ver3.py
@@ -0,0 +1,16 @@
+import sys
+
+try:
+    while True:
+        fib = [0,1,1,2,3,5,8,13,5,2,7,9,0,9,9,2,11,13,8,5,13,2,15,1]
+
+        # 入力に対応した数値を取り出す
+        N = int(input())
+        if N < 24:
+            print(fib[N])
+        else:
+            N = N % 24
+            print(fib[N])
+
+except EOFError:
+    pass
