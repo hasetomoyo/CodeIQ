1a090686e4a643649eac64aac7ba8dc0710a4ede
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-22 11:06:51 +0900

    標準入力の部分追加

--- kakakucom/cardgame.py
+++ kakakucom/cardgame.py
@@ -1,6 +1,15 @@
+import sys
+
 try:
     while True:
+        Total_point_of_opponent = int(input())
 
+        card_pattern = []
+        for line in range(sys.stdin):
+            card_pattern.append(line)
+
+        print(Total_point_of_opponent)
+        print(card_pattern)
 
 except EOFError:
     pass
