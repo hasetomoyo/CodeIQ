ce984bac083abce723be4705d25b675e238d8cff
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-22 11:52:11 +0900

    itertools使う版 うまく使えないので、input()をforでまわす式で作り直すため、バージョン保存

--- kakakucom/cardgame.py
+++ kakakucom/cardgame.py
@@ -1,15 +1,45 @@
 import sys
+import itertools
 
 try:
     while True:
-        Total_point_of_opponent = int(input())
+        # 対戦相手の合計点
+        total_point_of_opponent = int(input())
 
+        # 手元のカードの点数
         card_pattern = []
-        for line in range(sys.stdin):
-            card_pattern.append(line)
+        while True:
+            n = input()
+            if n == '':
+                break
+            card_pattern.append(n)
 
-        print(Total_point_of_opponent)
-        print(card_pattern)
+        print('相手のカード合計:', total_point_of_opponent)
+        print('手もとのカード:', card_pattern)
+        print('手元のカード数', len(card_pattern))
+
+        count = 0
+
+        if len(card_pattern) == 1:
+            if total_point_of_opponent < card_pattern[0]:
+                count = count + 1
+        else:
+            card_combination = []
+            for i in range(1, len(card_pattern)+1):
+                # 組み合わせを計算
+                # card_combination.extend(itertools.combinations(card_pattern, i))
+                card_combination = list(itertools.combinations(card_pattern, i))
+                print(card_combination)
+
+            # for j in range(0, len(card_combination)):
+            #     print(card_combination(j))
+
+
+
+
+
+
+
 
 except EOFError:
     pass
